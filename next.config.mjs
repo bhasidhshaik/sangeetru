af07b2c997041387a1368bc3955a12d532dfcdfc
bhasidhshaik <basidhshaik85@gmail.com>
2025-07-31 19:14:20 +0530

    Updated image ur

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    domains: ['harmonicsurvey.com'], // <-- replace with the actual domain
+  },
+}
+
+module.exports = nextConfig
 
 export default nextConfig;
